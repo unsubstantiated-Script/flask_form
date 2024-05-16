af797d4d75651e5c48be66395c90d0f31158250d
unsubstantiated-Script <jusfulton@gmail.com>
2024-05-15 21:00:13 -0700

    SQLite Wired Up

--- app.py
+++ app.py
@@ -1,6 +1,25 @@
+
+import os
+from datetime import datetime
+
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
+db = SQLAlchemy(app)
+
+
+# How to make models in Flask
+class Form(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(80))
+    last_name = db.Column(db.String(80))
+    email = db.Column(db.String(80))
+    date = db.Column(db.Date)
+    occupation = db.Column(db.String(80))
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -10,9 +29,21 @@ def index():
         last_name = request.form['last_name']
         email = request.form['email']
         date = request.form['date']
+        date_obj = datetime.strptime(date, '%Y-%m-%d')
         occupation = request.form['occupation']
+
+        # Building the form object
+        form = Form(first_name=first_name, last_name=last_name, email=email, date=date_obj, occupation=occupation)
+
+        # Sending it to the DB
+        db.session.add(form)
+        db.session.commit()
 
     return render_template("index.html")
 
 
-app.run(debug=True, port=5001)
+# Getting set to run this jazz as a script
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+        app.run(debug=True, port=5001)
